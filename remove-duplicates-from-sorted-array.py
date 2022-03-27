fa4cccc20096c9a39a9e59dd0671c6c5ee7b62ee
AndrewWigginCout <91389007+AndrewWigginCout@users.noreply.github.com>
2022-03-27 14:30:06 -0700

    Update remove-duplicates-from-sorted-array.py

--- remove-duplicates-from-sorted-array.py
+++ remove-duplicates-from-sorted-array.py
@@ -1,4 +1,9 @@
-#https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/
+'''https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/
+
+Runtime: 172 ms, faster than 25.14% of Python3 online submissions for Remove Duplicates from Sorted Array.
+Memory Usage: 15.6 MB, less than 67.31% of Python3 online submissions for Remove Duplicates from Sorted Array.
+Next challenges: 
+'''
 class Solution:
     def removeDuplicates(self, nums):
         nz=len(nums)
