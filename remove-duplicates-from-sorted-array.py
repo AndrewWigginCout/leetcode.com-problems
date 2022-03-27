9b503cf010603dafc87bc93b9a086df468ad782b
AndrewWigginCout <91389007+AndrewWigginCout@users.noreply.github.com>
2022-03-27 14:33:32 -0700

    Update remove-duplicates-from-sorted-array.py
    start at 1, check for edge case len <=1

--- remove-duplicates-from-sorted-array.py
+++ remove-duplicates-from-sorted-array.py
@@ -1,15 +1,15 @@
 '''https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/
 
-Runtime: 172 ms, faster than 25.14% of Python3 online submissions for Remove Duplicates from Sorted Array.
+Runtime: 160 ms, faster than 31.68% of Python3 online submissions for Remove Duplicates from Sorted Array.
 Memory Usage: 15.6 MB, less than 67.31% of Python3 online submissions for Remove Duplicates from Sorted Array.
-Next challenges: 
 '''
 class Solution:
     def removeDuplicates(self, nums):
         nz=len(nums)
-        i=0
-        j=0
-        while j<nz:
+        if nz<=1: return nz
+        i=1
+        j=1
+        while j<nz: 
             if nums[j] in nums[:i]:
                 j+=1
             else:
